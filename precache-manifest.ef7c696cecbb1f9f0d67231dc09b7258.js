234277c9b071367ab91d2d7a9378bf062c36a5df
Joaquin Cicetti <joaquincicetti@gmail.com>
2020-09-01 15:13:42 -0300

    Updates

---
+++ precache-manifest.ef7c696cecbb1f9f0d67231dc09b7258.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f153a999f58242260d98a0e6b8c8c5f2",
+    "url": "/cv-maker/index.html"
+  },
+  {
+    "revision": "e83386257823fc0d9b89",
+    "url": "/cv-maker/static/css/main.10e61c71.chunk.css"
+  },
+  {
+    "revision": "9043f4953d780cd82c06",
+    "url": "/cv-maker/static/js/2.08013242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-maker/static/js/2.08013242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83386257823fc0d9b89",
+    "url": "/cv-maker/static/js/main.5aec1137.chunk.js"
+  },
+  {
+    "revision": "d9a5075b48abfd890f05",
+    "url": "/cv-maker/static/js/runtime-main.de3b8720.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/cv-maker/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2a05bbfed2bab7389937b8a7c05f9663",
+    "url": "/cv-maker/static/media/aati-logo.2a05bbfe.png"
+  },
+  {
+    "revision": "11a0f3bc9afc8ea74f3a337086065bd6",
+    "url": "/cv-maker/static/media/logo.11a0f3bc.png"
+  },
+  {
+    "revision": "059016579a39efdcef0ce1c3f3a2559e",
+    "url": "/cv-maker/static/media/paula.05901657.png"
+  },
+  {
+    "revision": "71c29a43f3a5165c2c77b54f80f66eda",
+    "url": "/cv-maker/static/media/rosario.71c29a43.png"
+  }
+]);
